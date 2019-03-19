dc047ad2f08c860c2ae4d9adfd2d48524c57c9be
Tyson Hester <ironman9967@gmail.com>
2019-03-19 14:43:28 +0000

    call to view working

--- src/service/index.js
+++ src/service/index.js
@@ -4,6 +4,11 @@ import path from 'path'
 import Hapi from 'hapi'
 import Inert from 'inert'
 
+import { get, map } from 'lodash/fp'
+
+import FormData from 'form-data'
+import fetch from 'node-fetch'
+
 const server = Hapi.server({
 	port: process.env.PORT || 8000,
 	routes: {
@@ -12,6 +17,54 @@ const server = Hapi.server({
 		}
 	}
 })
+
+/*
+{
+	radius,
+	range,
+	to,
+	from,
+	max_results,
+	max_distance,
+	min_value,
+	use_mapping_value
+}
+*/
+
+const spanshApiRoute = 'https://spansh.co.uk/api'
+
+const getJobResults = ({ job }) => new Promise(resolve => setTimeout(() => {
+	console.log(`calling for ${job} - ${Date.now()}`)
+	fetch(`${spanshApiRoute}/results/${job}`).then(res => resolve(res.json()))
+}, 10000))
+
+const getRoute = ({ job }) => getJobResults({ job })
+	.then(res => res.status == 'ok'
+		? Promise.resolve(res.result)
+		: getJobResults({ job }))
+	.then(({ result }) => map(system => system)(result))
+		
+const view = ({ query }) => {
+	const form = new FormData()
+	Object.keys(query).forEach(k => form.append(k, query[k]))
+	return fetch(`${spanshApiRoute}/riches/route`, {
+		method: 'POST',
+		body: form,
+		headers: form.getHeaders()
+	})
+	.then(res => res.json())
+	.then(getRoute)
+}
+		
+const save = ({ name, query }) => view({ query })
+	.then(console.log)
+
+const api = {
+	'r2r-route': {
+		view,
+		save
+	}
+}
 
 server.register(Inert)
 .then(() => {
@@ -31,9 +84,22 @@ server.register(Inert)
 	server.route({
 		method: 'GET',
 		path: '/api/{route*}',
-		handler: ({ params: { route } }, h) => h.response().code(route
-			? console.log(route.split('/'))
-			: 404)
+		handler: ({ params: { route }, query }, h) => {
+			if (route) {
+				const p = route.replace('/', '.')
+				const m = get(p)(api)
+				if (m) {
+					return m({ query }).then(res => h.response(res))
+				}
+				else {
+					console.log('ROUTE NOT FOUND!\n', m, p, api)
+				}
+			}
+			else {
+				console.log('ROUTE NOT FOUND 2!\n', route)
+			}
+			return h.response().code(404)
+		}
 	})
 	
 	return server.start()
