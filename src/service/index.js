28643ffa27d303a1f88a3d503c84ef4bbada72a9
Tyson Hester <ironman9967@gmail.com>
2019-03-19 19:22:17 +0000

    view, save, delete, route and routes working

--- src/service/index.js
+++ src/service/index.js
@@ -4,10 +4,12 @@ import path from 'path'
 import Hapi from 'hapi'
 import Inert from 'inert'
 
-import { get, map } from 'lodash/fp'
+import { get } from 'lodash/fp'
 
 import FormData from 'form-data'
 import fetch from 'node-fetch'
+
+import redis from 'redis'
 
 const server = Hapi.server({
 	port: process.env.PORT || 8000,
@@ -18,31 +20,69 @@ const server = Hapi.server({
 	}
 })
 
-/*
-{
-	radius,
-	range,
-	to,
-	from,
-	max_results,
-	max_distance,
-	min_value,
-	use_mapping_value
-}
-*/
-
 const spanshApiRoute = 'https://spansh.co.uk/api'
+const redisGlobalPrepend = 'spansh-r2r-saver'
 
-const getJobResults = ({ job }) => new Promise(resolve => setTimeout(() => {
-	console.log(`calling for ${job} - ${Date.now()}`)
-	fetch(`${spanshApiRoute}/results/${job}`).then(res => resolve(res.json()))
-}, 10000))
+const getRoute = ({ job }) => new Promise(resolve => 
+	setTimeout(() => fetch(`${spanshApiRoute}/results/${job}`)
+		.then(res => res.json())
+		.then(({ status, result }) => resolve(status == 'queued'
+			? getRoute({ job })
+			: result))
+	, 50)
+)
 
-const getRoute = ({ job }) => getJobResults({ job })
-	.then(res => res.status == 'ok'
-		? Promise.resolve(res.result)
-		: getJobResults({ job }))
-	.then(({ result }) => map(system => system)(result))
+const convertNameToId = ({ name }) => name.replace(/ /g, '#').replace(/\./g, '$')
+
+const getRedisClient = opts => new Promise(resolve => {
+	const rc = redis.createClient(opts)
+	rc.once('ready', () => resolve(rc))
+})
+
+const addToSetWithClient = ({ rc }) => ({
+	key,
+	value
+}) => new Promise((resolve, reject) => rc.sadd(key, value, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
+
+const setStringWithClient = ({ rc }) => ({
+	key,
+	value
+}) => new Promise((resolve, reject) => rc.set(key, value, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
+	
+const getSetMemebersWithClient = ({ rc }) => ({ 
+	key 
+}) => new Promise((resolve, reject) => rc.smembers(key, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
+
+const getStringWithClient = ({ rc }) => ({
+	key
+}) => new Promise((resolve, reject) => rc.get(key, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
+	
+const removeFromSetWithClient = ({ rc }) => ({
+	key,
+	value
+}) => new Promise((resolve, reject) => rc.srem(key, value, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
+	
+const keysWithClient = ({ rc }) => ({
+	search
+}) => new Promise((resolve, reject) => rc.keys(search, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
+	
+const delKeyWithClient = ({ rc }) => ({
+	key
+}) => new Promise((resolve, reject) => rc.del(key, (err, reply) => err
+	? reject(err)
+	: resolve(reply)))
 		
 const view = ({ query }) => {
 	const form = new FormData()
@@ -54,15 +94,278 @@ const view = ({ query }) => {
 	})
 	.then(res => res.json())
 	.then(getRoute)
+	.then(route => {
+		const { name: origin } = route.shift()
+		const { name: destination } = route.pop()
+		return {
+			created: Date.now(),
+			origin,
+			destination,
+			settings: query,
+			systems: route.map(({ bodies, name, ...system }) => ({
+				id: convertNameToId({ name }),
+				name,
+				...system,
+				bodies: bodies.map(({ id, name, ...body }) => ({
+					id,
+					name,
+					complete: false,
+					...body
+				}))
+			}))
+		}
+	})
 }
 		
-const save = ({ name, query }) => view({ query })
-	.then(console.log)
+const save = ({ query: { name, ...query } }) => {
+	if (name) {
+		return view({ query })
+		.then(rawRoute => new Promise(resolve => {
+			const route = {
+				id: convertNameToId({ name }),
+				name,
+				...rawRoute
+			}
+			getRedisClient()
+			.then(rc => {
+				const addToSet = addToSetWithClient({ rc })
+				const setString = setStringWithClient({ rc })
+				addToSet({
+					key: `${redisGlobalPrepend}.routes`,
+					value: route.id
+				})
+				.then(() => Promise.all(Object.keys(route).reduce((promises, k) => {
+					if (![
+						'systems',
+						'settings'
+					].includes(k)) {
+						promises.push(setString({
+							key: `${redisGlobalPrepend}.route.${route.id}.${k}`,
+							value: route[k]
+						}))
+					}
+					return promises
+				}, [])))
+				.then(() => Promise.all(Object.keys(route.settings).map(k => 
+					setString({
+						key: `${redisGlobalPrepend}.route.${route.id}.setting.${k}`,
+						value: route.settings[k]
+					})
+				)))
+				.then(() => Promise.all(route.systems.map(system => 
+					addToSet({
+						key: `${redisGlobalPrepend}.route.${route.id}.systems`,
+						value: system.id
+					})
+					.then(() => setString({
+						key: `${redisGlobalPrepend}.route.${route.id}.system.${system.id}.name`,
+						value: system.name
+					}))
+					.then(() => setString({
+						key: `${redisGlobalPrepend}.route.${route.id}.system.${system.id}.jumps`,
+						value: system.jumps
+					}))
+					.then(() => Promise.all(system.bodies.map(body => 
+						addToSet({
+							key: `${redisGlobalPrepend}.route.${route.id}.system.${system.id}.bodies`,
+							value: body.id
+						})
+						.then(() => Promise.all(Object.keys(body).map(k =>
+							setString({
+								key: `${redisGlobalPrepend}.route.${route.id}.system.${system.id}.body.${body.id}.${k}`,
+								value: body[k]
+							})
+						)))
+					)))
+				)))
+				.then(() => {
+					rc.quit()
+					resolve(route)
+				})
+			})
+		}))
+	}
+	return Promise.resolve({ error: 'name is required' })
+}
+		
+const routes = () => getRedisClient()
+.then(rc => {
+	const getSetMembers = getSetMemebersWithClient({ rc })
+	const getString = getStringWithClient({ rc })
+	return getSetMembers({ key: `${redisGlobalPrepend}.routes` })
+	.then(routes => Promise.all(routes.map(routeId => getString({
+		key: `${redisGlobalPrepend}.route.${routeId}.name`
+	}))))
+	.then(routes => {
+		rc.quit()
+		return routes
+	})
+})
+		
+const route = ({ query: { name } }) => {
+	if (name) {
+		return getRedisClient()
+		.then(rc => {
+			const routeId = convertNameToId({ name })
+			const getSetMembers = getSetMemebersWithClient({ rc })
+			const getString = getStringWithClient({ rc })
+			return Promise.all([
+				'name',
+				'created',
+				'origin',
+				'destination'
+			].map(k => getString({
+				key: `${redisGlobalPrepend}.route.${routeId}.${k}`
+			})))
+			.then(([
+				name,
+				created,
+				origin,
+				destination
+			]) => ({
+				id: routeId,
+				name,
+				created,
+				origin,
+				destination
+			}))
+			.then(route => Promise.all([
+					'radius',
+					'range',
+					'to',
+					'from',
+					'max_results',
+					'max_distance',
+					'min_value',
+					'use_mapping_value'
+				].map(k => getString({
+					key: `${redisGlobalPrepend}.route.${routeId}.setting.${k}`
+				})))
+				.then(([
+					radius,
+					range,
+					to,
+					from,
+					max_results,
+					max_distance,
+					min_value,
+					use_mapping_value
+				]) => ({
+					...route,
+					settings: {
+						radius,
+						range,
+						to: to ? to : from,
+						from,
+						max_results,
+						max_distance,
+						min_value,
+						use_mapping_value
+					}
+				}))
+			)
+			.then(route => getSetMembers({
+					key: `${redisGlobalPrepend}.route.${routeId}.systems`
+				})
+				.then(systems => Promise.all(systems.map(systemId => Promise.all([
+						'name',
+						'jumps'
+					].map(k => getString({
+						key: `${redisGlobalPrepend}.route.${routeId}.system.${systemId}.${k}`
+					})))
+					.then(([
+						name,
+						jumps
+					]) => ({
+						id: systemId,
+						name,
+						jumps
+					}))
+					.then(system => getSetMembers({
+							key: `${redisGlobalPrepend}.route.${routeId}.system.${systemId}.bodies`
+						})
+						.then(bodies => Promise.all(bodies.map(bodyId => Promise.all([
+							'name',
+							'complete',
+							'distance_to_arrival',
+							'estimated_mapping_value',
+							'estimated_scan_value',
+							'is_terraformable',
+							'subtype',
+							'type'
+						].map(k => getString({
+							key: `${redisGlobalPrepend}.route.${routeId}.system.${systemId}.body.${bodyId}.${k}`
+						})))
+						.then(([
+							name,
+							complete,
+							distance_to_arrival,
+							estimated_mapping_value,
+							estimated_scan_value,
+							is_terraformable,
+							subtype,
+							type
+						]) => ({
+							id: bodyId,
+							name,
+							complete,
+							distance_to_arrival,
+							estimated_mapping_value,
+							estimated_scan_value,
+							is_terraformable,
+							subtype,
+							type
+						}))))
+						.then(bodies => ({
+							...system,
+							bodies
+						})))
+					)))
+					.then(systems => ({
+						...route,
+						systems
+					}))
+			))
+			.then(route => {
+				rc.quit()
+				return route
+			})
+		})
+	}
+	return Promise.resolve({ error: 'name is required' })
+}
+
+const deleteRoute = ({ query: { name } }) => {
+	if (name) {
+		return getRedisClient()
+		.then(rc => {
+			const routeId = convertNameToId({ name })
+			const removeFromSet = removeFromSetWithClient({ rc })
+			const keys = keysWithClient({ rc })
+			const delKey = delKeyWithClient({ rc })
+			return removeFromSet({
+				key: `${redisGlobalPrepend}.routes`,
+				value: routeId
+			})
+			.then(() => keys({
+				search: `${redisGlobalPrepend}.route.${routeId}.*`
+			}))
+			.then(keys => Promise.all(keys.map(key => delKey({
+				key
+			}))))
+			.then(() => { rc.quit() })
+		})
+	}
+	return Promise.resolve({ error: 'name is required' })
+}
 
 const api = {
 	'r2r-route': {
 		view,
-		save
+		save,
+		routes,
+		route,
+		'delete-route': deleteRoute
 	}
 }
 
@@ -86,7 +389,7 @@ server.register(Inert)
 		path: '/api/{route*}',
 		handler: ({ params: { route }, query }, h) => {
 			if (route) {
-				const p = route.replace('/', '.')
+				const p = route.replace(/\//g, '.')
 				const m = get(p)(api)
 				if (m) {
 					return m({ query }).then(res => h.response(res))
